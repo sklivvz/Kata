c42868cfb460d2f13bdb6f9bcf53ce0c2852ad93
Marco Cecconi <marco.cecconi@gmail.com>
2011-11-28 17:14:13 +0000

    Started KataPotter

---
+++ Kata.Tests/PotterBasketTests.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+
+namespace Kata.Tests
+{
+    /// <summary>
+    /// See: http://codingdojo.org/cgi-bin/wiki.pl?KataPotter
+    /// </summary>
+    [TestFixture]
+    public class PotterBasketTests
+    {
+        [Test]
+        [TestCase("Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Chamber of Secrets")]
+        [TestCase("Harry Potter and the Prisoner of Azkaban")]
+        [TestCase("Harry Potter and the Goblet of Fire")]
+        [TestCase("Harry Potter and the Order of the Phoenix")]
+        public void OneCopyOfAnyOfTheFiveBooksCosts8Eur(string book)
+        {
+            var basket = SetupBasket();
+
+            basket.Add(book);
+
+            Assert.AreEqual(8M, basket.Total);
+        }
+
+        [Test]
+        [TestCase("Harry Potter and the Chamber of Secrets", "Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Goblet of Fire", "Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Order of the Phoenix", "Harry Potter and the Philosopher's Stone")]
+        public void IfYouBuyTwoDifferentBooksYouGetA5PercentDiscountOnThoseTwoBooks(string book1, string book2)
+        {
+            var basket = SetupBasket();
+
+            basket.Add(book1);
+            basket.Add(book2);
+
+            Assert.AreEqual(8M * 2 * 0.95M, basket.Total);
+        }
+
+        [Test]
+        [TestCase("Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Chamber of Secrets")]
+        [TestCase("Harry Potter and the Prisoner of Azkaban")]
+        [TestCase("Harry Potter and the Goblet of Fire")]
+        [TestCase("Harry Potter and the Order of the Phoenix")]
+        public void TwoCopiesOfTheSameBookCosts16Eur(string book)
+        {
+            var basket = SetupBasket();
+
+            basket.Add(book);
+            basket.Add(book);
+
+            Assert.AreEqual(16M, basket.Total);
+        }
+
+
+        [Test]
+        [TestCase("Harry Potter and the Chamber of Secrets", "Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Goblet of Fire", "Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Order of the Phoenix", "Harry Potter and the Philosopher's Stone")]
+        public void IfYouBuyTwoDifferentBooksYouGetA5PercentDiscountOnThoseTwoBooksOnly(string book1, string book2)
+        {
+            var basket = SetupBasket();
+
+            basket.Add(book1);
+            basket.Add(book2);
+            basket.Add(book1);
+
+            Assert.AreEqual(8M * 2 * 0.95M + 8M, basket.Total);
+        }
+
+        [Test]
+        [TestCase("Harry Potter and the Chamber of Secrets", "Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Goblet of Fire", "Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Order of the Phoenix", "Harry Potter and the Philosopher's Stone")]
+        public void IfYouBuyTwoCopiesOfTwoDifferentBooksYouGetA5PercentDiscountOnThoseTwoBooksTwice(string book1, string book2)
+        {
+            var basket = SetupBasket();
+
+            basket.Add(book1);
+            basket.Add(book2);
+            basket.Add(book2);
+            basket.Add(book1);
+
+            Assert.AreEqual(8M * 4 * 0.95M, basket.Total);
+        }
+
+        [Test]
+        [TestCase("Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Chamber of Secrets", "Harry Potter and the Goblet of Fire", "Harry Potter and the Philosopher's Stone")]
+        [TestCase("Harry Potter and the Chamber of Secrets", "Harry Potter and the Order of the Phoenix", "Harry Potter and the Philosopher's Stone")]
+        public void IfYouBuyThreeDifferentBooksYouGetA10PercentDiscountOnThoseThreeBooks(string book1, string book2, string book3)
+        {
+            var basket = SetupBasket();
+
+            basket.Add(book1);
+            basket.Add(book2);
+            basket.Add(book3);
+
+            Assert.AreEqual(8M * 3 * 0.9M, basket.Total);
+        }
+
+
+        private PotterBasket SetupBasket()
+        {
+            var basket = new PotterBasket();
+            return basket;
+        }
+    }
+
+
+}
---
+++ Kata/PotterBasket.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+
+namespace Kata
+{
+    /// <summary>
+    /// Once upon a time there was a series of 5 books about a very English hero called Harry. 
+    /// (At least when this Kata was invented, there were only 5. Since then they have multiplied) 
+    /// Children all over the world thought he was fantastic, and, of course, so did the publisher. 
+    /// So in a gesture of immense generosity to mankind, (and to increase sales) 
+    /// they set up the following pricing model to take advantage of Harry's magical powers.
+    /// </summary>
+    public class PotterBasket
+    {
+        private readonly Dictionary<string, int> _cart = new Dictionary<string, int>();
+
+        public decimal Total
+        {
+            get
+            {
+                decimal ret = 0M;
+                while (_cart.Count > 0)
+                {
+                    var first = PopFirstElement();
+                    var second = FindDifferent(first);
+
+                    if (second != string.Empty)
+                    {
+                        Remove(second);
+
+                        var third = FindDifferent(first, second);
+                        if (third != string.Empty)
+                        {
+                            Remove(third);
+                            ret += 8M*3*0.9M;
+                        }
+                        else
+                        {
+                            ret += 8M*2*0.95M;
+                        }
+                    }
+                    else
+                    {
+                        ret += 8M;
+                    }
+                }
+                return ret;
+            }
+        }
+
+        private string FindDifferent(string book1, string book2)
+        {
+            string next = string.Empty;
+
+            foreach (string key in _cart.Keys)
+            {
+                if (key == book1) continue;
+                if (key == book2) continue;
+                next = key;
+                break;
+            }
+            return next;
+        }
+
+        private string FindDifferent(string book)
+        {
+            string next = string.Empty;
+
+            foreach (string key in _cart.Keys)
+            {
+                if (key == book) continue;
+                next = key;
+                break;
+            }
+            return next;
+        }
+
+        private string[] BooksInCart()
+        {
+            var keys = new string[_cart.Count];
+            _cart.Keys.CopyTo(keys, 0);
+            return keys;
+        }
+
+        private string PopFirstElement()
+        {
+            string firstElement = BooksInCart()[0];
+
+            Remove(firstElement);
+
+            return firstElement;
+        }
+
+        private void Remove(string book)
+        {
+            _cart[book]--;
+            if (_cart[book] == 0)
+                _cart.Remove(book);
+        }
+
+        public void Add(string book)
+        {
+            if (_cart.ContainsKey(book))
+                _cart[book]++;
+            else
+                _cart[book] = 1;
+        }
+    }
+}
